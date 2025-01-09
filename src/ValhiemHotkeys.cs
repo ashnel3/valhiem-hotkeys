a93d5aefa8090dc22839e7222661a2c2a89099f1
ashnel3 <40026133+ashnel3@users.noreply.github.com>
2025-01-09 14:20:38 -0800

    feat: initial commit

---
+++ src/ValhiemHotkeys.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ValhiemHotkeys {
+    [BepInPlugin(UID, NAME, VERSION)]
+    public class ValhiemHotkeys : BaseUnityPlugin {
+        const string UID = "com.ashnel3.ValhiemHotkeys";
+        const string NAME = "Valhiem Hotkeys";
+        const string VERSION = "0.1.0";
+
+        private readonly ConfigEntry<KeyboardShortcut> ConfigQuit;
+        private readonly ConfigEntry<KeyboardShortcut> ConfigDisconnect;
+        private readonly Harmony Harmony = new Harmony(UID);
+
+        public ValhiemHotkeys() {
+            ConfigQuit = Config.Bind("General", "Quit", new KeyboardShortcut(KeyCode.Delete), "Quit Hotkey");
+            ConfigDisconnect = Config.Bind("General", "Disconnect", new KeyboardShortcut(KeyCode.End), "Disconnect Hotkey");
+        }
+
+        public void Awake() {
+            Harmony.PatchAll();
+        }
+
+        public void Update() {
+            if (ConfigDisconnect.Value.IsPressed()) {
+                Game.instance.Logout();
+            }
+            if (ConfigQuit.Value.IsPressed()) {
+                Application.Quit();
+            }
+        }
+    }
+}
